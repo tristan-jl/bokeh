3d404db2e7ad3d26555da44a8471d8f6c0a701a2
Tristan Laurens <tristanlaurens@hotmail.com>
2023-01-01 14:05:30 +0000

    Clean up and add new param sets

--- src/complex.rs
+++ src/complex.rs
@@ -1,5 +1,4 @@
 use image::{DynamicImage, GenericImage, GenericImageView, Pixel};
-use num::complex::ComplexFloat;
 use num::Complex;
 
 use crate::params::KernelParamSet;
@@ -20,6 +19,7 @@ fn complex_gaussian_kernel(r: f64, kernel_radius: usize, a: f64, b: f64) -> Vec<
     kernel
 }
 
+/// Build all the gaussian kernels and normalise w.r.t. params
 fn complex_gaussian_kernels(
     params: &KernelParamSet,
     r: f64,
@@ -162,9 +162,7 @@ pub fn bokeh_blur(
         })
         .collect::<Vec<_>>();
 
-    dbg!(&kernels);
-
-    let total_out = kernels
+    for (n, [r, g, b, a]) in kernels
         .iter()
         .map(|kernel| {
             let mut temp = vec![[Complex::new(0.0, 0.0); 4]; (w * h) as usize];
@@ -173,10 +171,6 @@ pub fn bokeh_blur(
             vertical_filter(&temp, &mut output, kernel, w, h);
             output
         })
-        .collect::<Vec<_>>();
-
-    for (n, [r, g, b, a]) in total_out
-        .iter()
         .enumerate()
         .fold(vec![[0.0; 4]; (w * h) as usize], |mut acc, (n, x)| {
             for (a, y) in acc.iter_mut().zip(x.iter()) {
@@ -190,82 +184,18 @@ pub fn bokeh_blur(
         .into_iter()
         .enumerate()
     {
-        /*
-        debug_assert!(
-            [r, g, b, a].iter().all(|&x| (0.0..256.0).contains(&x)),
-            "RGBA OOB: {} {} {} {}",
-            r,
-            g,
-            b,
-            a
-        );
-            */
+        // Clamp any values from floating point ops - ensure the cast to u8 is ok
         let r = r.clamp(0.0, 255.0) as u8;
         let g = g.clamp(0.0, 255.0) as u8;
         let b = b.clamp(0.0, 255.0) as u8;
         let a = a.clamp(0.0, 255.0) as u8;
 
-        let x = n as u32 % w;
-        let y = n as u32 / w;
-
-        unsafe { img.unsafe_put_pixel(x, y, *Pixel::from_slice(&[r, g, b, a])) }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    /*
-    #[test]
-    fn test_complex_kernel() {
-        let r = complex_gaussian(1.4, 4, 0.862325, 1.624835);
-        dbg!(r.iter().fold(0.0, |acc, x| acc + x.norm()).abs());
-        assert!((r.iter().fold(0.0, |acc, x| acc + x.norm()).abs() - 1.0) < 0.00000001);
-
-        let expected = [
-            Complex::new(-5.8840633e-06, -1.7689392e-06),
-            Complex::new(3.2656451e-03, 3.5559295e-03),
-            Complex::new(-5.4171007e-02, 2.5797081e-01),
-            Complex::new(1.0000000e+00, 0.0000000e+00),
-            Complex::new(-5.4171007e-02, 2.5797081e-01),
-            Complex::new(3.2656451e-03, 3.5559295e-03),
-            Complex::new(-5.8840633e-06, 1.7689392e-06),
-        ];
-        assert_eq!(r, &expected);
-    }
-    */
-    #[test]
-    fn test_complex_kernel() {
-        let r = complex_gaussian_kernel(1.4, 4, 0.862325, 1.624835);
-
-        /*
-        dbg!(r
-            .iter()
-            .map(|i| Complex::new(i.re as f64, i.im as f64))
-            .zip(r2.iter())
-            .map(|(i, j)| i - j)
-            .collect::<Vec<_>>());
-
-        assert_eq!(
-            r.iter()
-                .map(|i| Complex::new(i.re as f64, i.im as f64))
-                .collect::<Vec<_>>(),
-            r2
-        );
-        */
-
-        dbg!(r.iter().map(|i| i.norm()).collect::<Vec<_>>());
-
-        let expected = [
-            Complex::new(-5.8840633e-06, -1.7689392e-06),
-            Complex::new(3.2656451e-03, 3.5559295e-03),
-            Complex::new(-5.4171007e-02, 2.5797081e-01),
-            Complex::new(1.0000000e+00, 0.0000000e+00),
-            Complex::new(-5.4171007e-02, 2.5797081e-01),
-            Complex::new(3.2656451e-03, 3.5559295e-03),
-            Complex::new(-5.8840633e-06, 1.7689392e-06),
-        ];
-        assert_eq!(r, &expected);
+        unsafe {
+            img.unsafe_put_pixel(
+                n as u32 % w,
+                n as u32 / w,
+                *Pixel::from_slice(&[r, g, b, a]),
+            )
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use image::io::Reader as ImageReader;
 
 fn main() {
     let mut img = ImageReader::open("input.jpg").unwrap().decode().unwrap();
-    img.bokeh_blur(5.0, 50, &KERNEL5_PARAM_SET);
+    img.bokeh_blur(5.0, 100, &KERNEL9_PARAM_SET);
 
     img.save("output.png").unwrap();
 }
