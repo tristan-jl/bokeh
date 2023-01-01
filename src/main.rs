871d65b31b3b4ca1c655f496a743c4d7721828f6
Tristan Laurens <tristanlaurens@hotmail.com>
2023-01-01 14:52:43 +0000

    Basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::env;
+
 use bokeh::{params::*, Blur};
 use image::io::Reader as ImageReader;
 
 fn main() {
-    let mut img = ImageReader::open("input.jpg").unwrap().decode().unwrap();
-    img.bokeh_blur(5.0, 100, &KERNEL9_PARAM_SET);
+    let mut args = env::args();
+    args.next();
+    let input_path = args.next().unwrap_or_else(|| "input.png".to_owned());
+    let output_path = args.next().unwrap_or_else(|| "output.png".to_owned());
 
-    img.save("output.png").unwrap();
+    let mut img = ImageReader::open(input_path).unwrap().decode().unwrap();
+    img.bokeh_blur(2.0, 50, &KERNEL5_PARAM_SET);
+    img.save(output_path).unwrap();
 }
